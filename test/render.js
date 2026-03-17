9efa66597a8f01fa98c96724f5e7fc0315620945
Kane Shenton <jkshenton@gmail.com>
2026-03-17 22:08:28 +0000

    Polish renderer click guard cleanup

--- test/render.js
+++ test/render.js
@@ -5,12 +5,13 @@ import { Renderer } from '../lib/render.js';
 
 const expect = chai.expect;
 
-function dispatchPointerEvent(target, type, x, y) {
+function dispatchPointerEvent(target, type, x, y, { isPrimary = true } = {}) {
     const event = new window.MouseEvent(type, {
         bubbles: true,
         clientX: x,
         clientY: y,
     });
+    Object.defineProperty(event, 'isPrimary', { value: isPrimary });
     target.dispatchEvent(event);
 }
 
@@ -81,5 +82,87 @@ describe('Renderer click drag guard', function () {
         dispatchPointerEvent(canvas, 'pointerup', 100, 100);
 
         expect(raycastCalls).to.equal(0);
+    });
+
+    it('should ignore non-primary pointer events', function () {
+        let raycastCalls = 0;
+        renderer._raycastClick = () => {
+            raycastCalls++;
+        };
+
+        dispatchPointerEvent(renderer._r.domElement, 'pointerdown', 100, 100, { isPrimary: false });
+        dispatchPointerEvent(renderer._r.domElement, 'pointerup', 100, 100, { isPrimary: false });
+
+        expect(raycastCalls).to.equal(0);
+    });
+
+    it('should not raycast after pointercancel', function () {
+        let raycastCalls = 0;
+        renderer._raycastClick = () => {
+            raycastCalls++;
+        };
+
+        dispatchPointerEvent(renderer._r.domElement, 'pointerdown', 100, 100);
+        dispatchPointerEvent(renderer._r.domElement, 'pointercancel', 100, 100);
+        dispatchPointerEvent(renderer._r.domElement, 'pointerup', 100, 100);
+
+        expect(raycastCalls).to.equal(0);
+    });
+
+    it('should convert document coordinates using the canvas bounding rect', function () {
+        renderer._r.domElement.getBoundingClientRect = () => ({
+            left: 40,
+            top: 20,
+            width: 200,
+            height: 100,
+        });
+
+        const vector = renderer.documentToWorld(140, 70);
+
+        expect(vector.x).to.equal(0);
+        expect(vector.y).to.equal(0);
+    });
+
+    it('should reuse the shared NDC vector', function () {
+        renderer._r.domElement.getBoundingClientRect = () => ({
+            left: 0,
+            top: 0,
+            width: 320,
+            height: 240,
+        });
+
+        const first = renderer.documentToWorld(160, 120);
+        const second = renderer.documentToWorld(80, 60);
+
+        expect(second).to.equal(first);
+        expect(second.x).to.equal(-0.5);
+        expect(second.y).to.equal(0.5);
+    });
+
+    it('should reuse the shared raycaster instance', function () {
+        let setFromCameraCalls = 0;
+        const sharedRaycaster = {
+            setFromCamera(vector, camera) {
+                setFromCameraCalls++;
+                expect(camera).to.equal(renderer._c);
+                expect(vector.x).to.equal(0);
+                expect(vector.y).to.equal(0);
+            },
+            intersectObjects() {
+                return [];
+            },
+        };
+
+        renderer._r.domElement.getBoundingClientRect = () => ({
+            left: 0,
+            top: 0,
+            width: 320,
+            height: 240,
+        });
+        renderer._raycaster = sharedRaycaster;
+
+        renderer._raycastClick({ clientX: 160, clientY: 120 });
+
+        expect(setFromCameraCalls).to.equal(1);
     });
 });
